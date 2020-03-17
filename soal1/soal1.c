8dc1c4fdbfeb9e7d69e0895dc400f6a65278a8fc
Marsellaeve <marsella.eve@gmail.com>
2020-03-17 14:17:30 +0700

    revisi soal1

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -9,6 +9,8 @@
 #include <string.h>
 #include <time.h>
 #include <wait.h>
+#include<dirent.h>
+#include <string.h>
  
 int main(int argc, char** array){
   pid_t pid, sid;        // Variabel untuk menyimpan PID
@@ -48,16 +50,32 @@ int main(int argc, char** array){
 	int i,arr[10],j,temp,a,b;
 	char str[35];
 	for(i=1;i<argc;i++){ printf("%s",array[i]);}
-	if(argc<5){
-		printf("Program error masukkan argumen\n");
+	DIR *dir;
+	struct dirent *ent;
+	char text[200]="/home/evelyn/Praktikum2/";
+	int flags=0;
+	if ((dir = opendir (text)) != NULL) {
+	  while ((ent = readdir (dir)) != NULL) {
+		if(strcmp(ent->d_name,array[4])==0){flags=1; break;}
+	  }
+	if(flags==0){
+		printf("file tidak ditemukan\n");
+		return 0;
+	}		
+	} 
+	if(argc<5||argc>5){
+		printf("a Program error masukkan argumen\n");
 		return 0;
 	}
 	if(array[4][(strlen(array[4])-3)]!='.'|| array[4][(strlen(array[4])-2)]!='s'|| array[4][(strlen(array[4])-1)]!='h'){
 		printf("Program error masukkan argumen\n");
 		return 0;
 	}
+	int flagss=0;
 	for (i = 1; i < argc; ++i){		
-		if(strcmp(array[i],"*")==0) { arr[i-1]=-1; continue;}
+		if(strcmp(array[i],"*")==0) {
+			flagss++; if(flagss==3) return 0;
+			arr[i-1]=-1; continue;}
 		else if(i==4){
 			strcpy(str,array[i]);
 			continue;
@@ -68,7 +86,7 @@ int main(int argc, char** array){
 			a=a-48; b=b-48;  //printf("%d %d\n",a,b);
 			if(a<0||a>9||b<0||a>9){
 			printf("Program error, input harus angka\n");
-			break;
+			return 0;
 			}
 			temp=(a*10)+b;
 		}
@@ -76,7 +94,7 @@ int main(int argc, char** array){
 			temp=(int)(array[i][0])-48;
 			if(temp<0||temp>9){
 			printf("Program error, b input harus angka\n");
-			break;
+			return 0;
 			}
 		}
 		arr[i-1]=temp;
@@ -84,12 +102,12 @@ int main(int argc, char** array){
 		
 		if((i==1||i==2)&&(arr[i-1]<0||arr[i-1]>59)){
 		printf("Program error masukkan detik/menit(0-59)\n");
-		break;
+		return 0;
 		}
 		
 		else if(i==3&&(arr[i-1]>23||arr[i-1]<0)){
 		printf("c Program error masukkan jam (0-23)\n");
-		break;
+		return 0;
 		}
 	}
 	
